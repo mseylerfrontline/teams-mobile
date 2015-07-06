97c9e710cf851f29bbd2646cfaedb1819b47a906
Jack Guy <jack@techwiq.com>
2015-07-06 12:07:46 -0500

    Added platform add hook to automatically install plugins

---
+++ hooks/after_platform_add/010_install_plugins.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+//this hook installs all your plugins
+
+// add your plugins to this list--either
+// the identifier, the filesystem location
+// or the URL
+var pluginlist = [
+   "com.ionic.keyboard",
+   "com.phonegap.plugins.PushPlugin",
+   "cordova-plugin-console",
+   "cordova-plugin-device",
+   "cordova-plugin-geolocation",
+   "cordova-plugin-splashscreen",
+   "cordova-plugin-whitelist"
+];
+
+// no need to configure below
+
+var fs = require('fs');
+var path = require('path');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+function puts(error, stdout, stderr)
+{
+   console.log(stdout)
+}
+
+pluginlist.forEach(function(plug)
+{
+   exec("ionic plugin add " + plug, puts);
+});
